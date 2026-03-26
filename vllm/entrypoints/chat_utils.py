ef596d70da7e0b1d23fbcfda14d176d2e730b55e
Yun Wang <maoerpet@foxmail.com>
2026-03-26 17:59:29 +0800

    [Feature] Add support for video content retrieval in MM_PARSER_MAP

--- vllm/entrypoints/chat_utils.py
+++ vllm/entrypoints/chat_utils.py
@@ -1321,6 +1321,7 @@ MM_PARSER_MAP: dict[
     "input_audio": lambda part: _InputAudioParser(part).get("input_audio", None),
     "refusal": lambda part: _RefusalParser(part).get("refusal", None),
     "video_url": lambda part: _VideoParser(part).get("video_url", {}).get("url", None),
+    "video": lambda part: part.get("video", None),
 }
 
 
